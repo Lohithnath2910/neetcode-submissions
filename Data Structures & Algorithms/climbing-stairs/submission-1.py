f579ca0320e7f254b489b285cce82f76ae2f1ff7
Yaganti Lohithnath Reddy <lohithnath2910@gmail.com>
2026-08-02 17:33:40 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        ne = [0]*(n+1)
+        ne[0] = 1
+        ne[1] = 2
+
+        for i in range(2,n+1):
+            ne[i] = ne[i-1] + ne[i-2]
+        return ne[n-1]
